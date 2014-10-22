0ef4f433640cdb7b9808dea990cc65414ec8e293
maximo281 <maximo281@outlook.com>
2014-10-21 19:17:00 -0500

    front 01

---
+++ application/config/gsl_config.php
@@ -0,0 +1,9 @@
+<?php if(!defined ('BASEPATH')) exit('No se permite el acceso directo a el script');
+
+$config['website_name']=" :: Grupo Sin Limites ::";
+$config['webmaster_email']='';
+
+//template public 
+$config['head']='header_public_template';
+//$config['body']='body_public_template';
+$config['footer']='footer_public_template';
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -3,23 +3,20 @@
 class Welcome extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
+		function __construct()
+		{
+			parent::__construct();
+
+			$this->config->load('gsl_config');
+		}
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$body='welcome_message';
+		$this->load->view($this->config->item('head'));
+		$this->load->view($body);
+		$this->load->view($this->config->item('footer'));
+	
 	}
 }
 
